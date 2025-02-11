39749e306c2c0258275b5b2b084f01c650f55837
Valonekiil <149128324+Valonekiil@users.noreply.github.com>
2025-02-11 18:53:10 +0700

    apa ciikk
    jancok lah uabot engine babik

---
+++ Assets/Script/firstsquare.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firstsquare : MonoBehaviour
+{
+    [SerializeField]
+    public Transform pieceholder;
+    private Vector2 initposition;
+    private float deltaX, deltaY;
+    public static bool locked;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initposition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && !locked)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchpos = Camera.main.ScreenToWorldPoint(touch.position);
+
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchpos)) ;
+                    {
+                        deltaX = touchpos.x - transform.position.x;
+                        deltaY = touchpos.y - transform.position.y;
+                    }
+                    break;
+                case TouchPhase.Moved:
+                    if (GetComponent<Collider2D>() == Physics2D.OverlapPoint(touchpos)) ;
+                    {
+                        transform.position = new Vector2(touchpos.x - deltaX, touchpos.y - deltaY);
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    if (Mathf.Abs(transform.position.x - pieceholder.position.x) <= 0.5f &&
+                        Mathf.Abs(transform.position.y - pieceholder.position.y) <= 0.5f)
+                    {
+                        transform.position = new Vector2(pieceholder.position.x, pieceholder.position.y);
+                        locked = true;
+                    }
+                    else
+                    {
+                        transform.position = new Vector2(initposition.x, initposition.y);
+                    }
+                    break;
+            }
+        }
+    }
+}
